740ef2f83e471959d46cc45fbf2ae63f6187a3d0
torgodly <torgodly@gmail.com>
2023-12-27 20:14:46 +0200

    hide close strap

--- resources/views/components/bid-card.blade.php
+++ resources/views/components/bid-card.blade.php
@@ -3,12 +3,14 @@
 
 
     <div class="sticky  top-32 flex flex-col items-center gap-6  w-full">
-        <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 w-[420px] ">
-            <div
-                class="bg-gradient-to-r from-red-600 to-pink-500 text-white font-bold py-2 px-6 transform -rotate-[42deg] shadow-lg text-center text-xl capitalize rounded-3xl">
-                {{__('This Auction is Closed')}}
+        @if($auction->status === 'closed')
+            <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 w-[420px] ">
+                <div
+                    class="bg-gradient-to-r from-red-600 to-pink-500 text-white font-bold py-2 px-6 transform -rotate-[42deg] shadow-lg text-center text-xl capitalize rounded-3xl">
+                    {{__('This Auction is Closed')}}
+                </div>
             </div>
-        </div>
+        @endif
         <div
             class="max-w-xs w-full bg-white border border-gray-200 rounded-lg shadow  p-6 flex flex-col justify-center text-center drop-shadow-xl">
 
@@ -18,13 +20,13 @@
                 <x-text-input
                     :disabled="$auction->has_winner"
                     wire:model="bid" type="number"
-                              class="w-full h-12 mt-4 text-center text-2xl font-bold border border-gray-200 rounded-lg shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-                              required/>
+                    class="w-full h-12 mt-4 text-center text-2xl font-bold border border-gray-200 rounded-lg shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                    required/>
                 <x-input-error :messages="$errors->get('bid')" class="mt-2"/>
                 <x-primary-button
                     :disabled="$auction->has_winner"
                     class="w-full flex justify-center mt-4 h-12 capitalize !text-base !font-bold"
-                                  wire:click="Bid" wire:target="Bid" wire:loading.attr="disabled">
+                    wire:click="Bid" wire:target="Bid" wire:loading.attr="disabled">
                     <x-loading-indicator name="Bid"/>
                     {{__('Bid')}}
                 </x-primary-button>
@@ -32,7 +34,7 @@
                     <x-primary-button
                         :disabled="$auction->has_winner"
                         class="w-full flex justify-center  h-12 capitalize !text-base !font-bold"
-                                      wire:click="buyNow" wire:target="buyNow" wire:loading.attr="disabled">
+                        wire:click="buyNow" wire:target="buyNow" wire:loading.attr="disabled">
                         <x-loading-indicator name="buyNow"/>
                         {{__('Buy Now')}}
                     </x-primary-button>
