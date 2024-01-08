6dc8f34e5a1d7ef806ed5bc5355231b60cbb9395
torgodly <torgodly@gmail.com>
2024-01-08 11:33:39 +0200

    add category

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+use App\Models\Category;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255)->columnSpanFull()->translateLabel(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()->translateLabel(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make()
+                    ->modalWidth('md')
+                    ->modalHeading(__('Edit Category'))
+                    ->modalDescription(__('Edit this category'))
+                    ->modalSubmitActionLabel(__('Edit Category'))
+                    ->modalIcon('heroicon-o-tag')
+                    ->requiresConfirmation()
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make()
+                    ->modalWidth('md')
+                    ->modalHeading(__('Add Category'))
+                    ->modalDescription(__('Add a new category'))
+                    ->modalSubmitActionLabel(__('Add Category'))
+                    ->modalIcon('heroicon-o-tag')
+                    ->requiresConfirmation(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+//            'create' => Pages\CreateCategory::route('/create'),
+//            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
