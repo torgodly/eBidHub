0c362b97858249efd6218e57f2165e63a2a2ddd4
torgodly <torgodly@gmail.com>
2024-01-08 21:16:26 +0200

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,20 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(20)->create();
-        Auction::factory(1)->create()->each(function (Auction $auction) {
-            for ($i = 0; $i < 5; $i++) {
-                $auction->addMediaFromUrl('https://picsum.photos/1024/683')->toMediaCollection('Auctions');
-            }
-        });
-//        Bid::factory(50)->create();
-        \App\Models\Comment::factory(50)->create();
-        Category::factory(100)->create();
-
+//        \App\Models\User::factory(20)->create();
+//        Auction::factory(1)->create()->each(function (Auction $auction) {
+//            for ($i = 0; $i < 5; $i++) {
+//                $auction->addMediaFromUrl('https://picsum.photos/1024/683')->toMediaCollection('Auctions');
+//            }
+//        });
+////        Bid::factory(50)->create();
+//        \App\Models\Comment::factory(50)->create();
+//        Category::factory(100)->create();
+//
 
         \App\Models\User::factory()->create([
             'name' => 'Test User',
             'email' => 'admin@admin.com',
+            'type' => 'admin',
             'balance' => 100000,
         ]);
     }
