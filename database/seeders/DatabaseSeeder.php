4bbb69e97e90763c66b74b4439983ab166f8f0f4
torgodly <torgodly@gmail.com>
2024-01-09 23:17:09 +0200

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Auction;
-use App\Models\Bid;
-use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,21 +14,21 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 //        \App\Models\User::factory(20)->create();
-//        Auction::factory(1)->create()->each(function (Auction $auction) {
-//            for ($i = 0; $i < 5; $i++) {
-//                $auction->addMediaFromUrl('https://picsum.photos/1024/683')->toMediaCollection('Auctions');
-//            }
-//        });
-////        Bid::factory(50)->create();
+        Auction::factory(5)->create()->each(function (Auction $auction) {
+            for ($i = 0; $i < 5; $i++) {
+                $auction->addMediaFromUrl('https://picsum.photos/1024/683')->toMediaCollection('Auctions');
+            }
+        });
+//        Bid::factory(50)->create();
 //        \App\Models\Comment::factory(50)->create();
 //        Category::factory(100)->create();
 //
 
-        \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'admin@admin.com',
-            'type' => 'admin',
-            'balance' => 100000,
-        ]);
+//        \App\Models\User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'admin@admin.com',
+//            'type' => 'admin',
+//            'balance' => 100000,
+//        ]);
     }
 }
